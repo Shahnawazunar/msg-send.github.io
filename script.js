e618f12d9c5346865079724c39ab1af1cff3669b
Shahnawazunar <shahnawazunar85@gmail.com>
2023-12-20 16:33:23 +0500

    Just Dom Practice

---
+++ script.js
@@ -0,0 +1,10 @@
+const input = document.getElementById('input');
+const arr = [];
+const sendMassage=()=>{
+  
+let display =document.getElementById('output').innerHTML=
+ input.value;
+ input.value = ''
+let play =+ arr.push().input.value;
+console.log(play);
+}
